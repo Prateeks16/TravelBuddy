d954b0bea32f1e8cd6c925bca13e1d47c8039eac
Prateek Sahu <153312544+Prateeks16@users.noreply.github.com>
2024-12-28 23:34:27 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'fade-in-down': 'fade-in-down 0.5s ease-out',
+        'fade-in-up': 'fade-in-up 0.5s ease-out',
+      },
+    },
+  },
+  plugins: [],
+}
+
